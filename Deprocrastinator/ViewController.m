4eec0b6608d9eaa43309ccbd2f1acf13eceea71f
jenkelley <i.jen.kelley@gmail.com>
2015-03-16 14:51:27 -0500

    delete works

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -51,28 +51,30 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *tappedCell = [tableView cellForRowAtIndexPath:indexPath];
-    if (<#condition#>) {
-        <#statements#>
-    }
 
 
     tappedCell.textLabel.textColor = [UIColor greenColor];
 
-
-
-
-
 }
+
+
 - (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
-    sender.title = @"Done";
 
-
-
-
-
+    if ([sender.title isEqualToString:@"Edit"]) {
+        sender.title = @"Done";
+        [self.deprocrastinatorTableView setEditing:YES animated:YES];
+    } else {
+        sender.title = @"Edit";
+        [self.deprocrastinatorTableView setEditing:NO animated:YES];
+    }
 }
 
-
+- (void) tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle ==UITableViewCellEditingStyleDelete) {
+        [self.taskArray removeObjectAtIndex:indexPath.row];
+        [tableView reloadData];
+    }
+}
 
 
 
